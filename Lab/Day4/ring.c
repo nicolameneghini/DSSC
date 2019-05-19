b1c94d0d466235bf23e753174ce562496317d4ac
Nicola Meneghini <nicolameneghini@hotmail.it>
2019-05-19 09:33:35 +0200

    ...

--- Lab/Day4/ring.c
+++ Lab/Day4/ring.c
@@ -10,10 +10,12 @@ int main(int argc, char* argv[]){
  int rank = 0; // store the MPI identifier of the process
  int npes = 1; // store the number of MPI processes
  MPI_Request send_request, recv_request; 
+ MPI_Status status;
  
  MPI_Init( &argc, &argv );
  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
  MPI_Comm_size( MPI_COMM_WORLD, &npes );
+
  unsigned int i;
  int last = npes-1;
  int message;
@@ -23,7 +25,7 @@ int main(int argc, char* argv[]){
    message = 1;
  else
    message = 0;
-
+/*
  if(rank == 0){
  MPI_Send(&message,1,MPI_INT, last , 101,MPI_COMM_WORLD);
  }
@@ -33,23 +35,19 @@ int main(int argc, char* argv[]){
   fprintf( stderr, "\nI am process %d and my message is %d \n", rank, message );
   message += rank;
  }
-
- if(rank != 0){
-   MPI_Send(&message,1,MPI_INT, rank -1 , 101,MPI_COMM_WORLD);
+*/
+ for(i = 0; i <last; i++){
+   	 
+   MPI_Isend(&message,1,MPI_INT, (rank+1)%npes, 101,MPI_COMM_WORLD, &send_request);
+   //sum += rank ;
+   MPI_Recv(&message,1,MPI_INT, (rank-1+npes)%npes, 101, MPI_COMM_WORLD,  MPI_STATUS_IGNORE);
+   MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+   
  }
- if(rank != last){
-   MPI_Recv(&message,1,MPI_INT, rank +1, 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-   fprintf( stderr, "\nI am process %d and my message is %d \n", rank, message );
-   message += rank;
- }
+ 
+ fprintf( stderr, "I am process %d and my message is %d \n", rank, message ); 
 
- // MPI_Recv(&message,1,MPI_INT, 0, 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
- // message += rank;
- // MPI_Send(&message,1,MPI_INT, rank -1 , 101,MPI_COMM_WORLD);//, &send_request);
- // MPI_Recv(&message,1,MPI_INT, rank +1, 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
- // fprintf( stderr, "\nI am process %d and my message is %d \n", rank, message );
-
-  MPI_Finalize();
+ MPI_Finalize();
 
 
  return 0;
