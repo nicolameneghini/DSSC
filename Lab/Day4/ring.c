984b0f61080c71c1c00373ad863aebfe0f80f628
Nicola Meneghini <nicolameneghini@hotmail.it>
2019-05-26 11:26:11 +0200

    vectorial ring

--- Lab/Day4/ring.c
+++ Lab/Day4/ring.c
@@ -4,48 +4,57 @@
 #include <math.h>
 #include <mpi.h>
 
+#define N 100
+
+void swap(int**a, int**b)
+{ 
+  int*tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
 
 int main(int argc, char* argv[]){
 
  int rank = 0; // store the MPI identifier of the process
  int npes = 1; // store the number of MPI processes
- MPI_Request send_request, recv_request; 
- MPI_Status status;
  
  MPI_Init( &argc, &argv );
  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
  MPI_Comm_size( MPI_COMM_WORLD, &npes );
 
- unsigned int i;
+ MPI_Request request;
+ MPI_Status status;
+
+ unsigned int i, j;
  int last = npes-1;
- int message;
- int sum = 0;
+
+ int *message = (int*)malloc(sizeof(int)*N);
+ int *receive = (int*)calloc(sizeof(int),N);
+ int *sum = (int*)calloc(sizeof(int),N);
  
- if(rank == 0)
-   message = 1;
- else
-   message = 0;
-/*
- if(rank == 0){
- MPI_Send(&message,1,MPI_INT, last , 101,MPI_COMM_WORLD);
- }
+ for(i = 0; i < N; i++) message[i] = rank;
  
- if(rank == last){
-  MPI_Recv(&message,1,MPI_INT, 0, 101, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  fprintf( stderr, "\nI am process %d and my message is %d \n", rank, message );
-  message += rank;
- }
-*/
+ 
  for(i = 0; i <last; i++){
    	 
-   MPI_Isend(&message,1,MPI_INT, (rank+1)%npes, 101,MPI_COMM_WORLD, &send_request);
-   //sum += rank ;
-   MPI_Recv(&message,1,MPI_INT, (rank-1+npes)%npes, 101, MPI_COMM_WORLD,  MPI_STATUS_IGNORE);
-   MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+   MPI_Isend(message,N,MPI_INT, (rank+1)%npes, 101,MPI_COMM_WORLD, &request);
    
+   for(j = 0; j < N; j++) sum[j] += message[j] ;
+   
+   MPI_Recv(receive,N,MPI_INT, (rank-1+npes)%npes, 101, MPI_COMM_WORLD,  MPI_STATUS_IGNORE);
+   MPI_Wait(&request, &status);
+   swap(&receive, &message);
+   //for(j = 0; j < N; j++) sum[j] = receive[j];
  }
  
- fprintf( stderr, "I am process %d and my message is %d \n", rank, message ); 
+ fprintf( stderr, "I am process %d and my message is %d \n", rank, sum[0]);
+ 
+ MPI_Barrier(MPI_COMM_WORLD);
+ 
+ free(message);
+ free(receive);
+ free(sum); 
 
  MPI_Finalize();
 
